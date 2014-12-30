90827f7a89952d6b35b32348731fb29f11baf346
bobmin <maik@btmx.net>
2014-12-30 16:42:38 +0100

    - Startparam für BBMC und BOB (Standard)

--- BobTool/start.bat
+++ BobTool/start.bat
@@ -1,9 +1,18 @@
 @echo off
 set GIT_WORK_PATH=D:\Eclipse\git\bobtool
 set BOB_TOOL_PATH=%GIT_WORK_PATH%\BobTool\bin
+set BOB_DEMO_PATH=%GIT_WORK_PATH%\BobDemo\bin
 set BBMC_TOOL_PATH=%GIT_WORK_PATH%\BbmcTool\bin
-set START_CLASS_PATH=%BOB_TOOL_PATH%;%BBMC_TOOL_PATH%
+REM Auswahl Konfiguration
+if "%1" == "bbmc" (
+	echo Konfiguration BBMC 
+	set START_CLASS_PATH=%BOB_TOOL_PATH%;%BBMC_TOOL_PATH%
+) else (
+	echo Konfiguration DEMO
+	set START_CLASS_PATH=%BOB_TOOL_PATH%;%BOB_DEMO_PATH%
+)
+REM Starten!
 echo BobTool-STARTER...
-echo classpath: %START_CLASS_PATH%
+echo [%1] classpath: %START_CLASS_PATH%
 java -cp "%START_CLASS_PATH%" bob.tool.BtMain
  
