8cc8d5b1288533d7ed4f11669acae8ddc91b6db7
maikboettcher <maikboettcher@NB-MBOETTCHER02.bur-kg.org>
2017-08-12 22:33:00 +0200

    dummy test added

---
+++ csvexport/test/bob/gadget/csvexport/DummyTest.java
@@ -0,0 +1,13 @@
+package bob.gadget.csvexport;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void testDummy() {
+		Assert.assertTrue(true);
+	}
+
+}
